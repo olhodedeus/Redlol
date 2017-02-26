ebf1868141938f09fb550fbe6f2b95032961bd0b
Olho de Deus <portaltelezap@gmail.com>
2017-02-25 21:23:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-read -p "Do you want me to install Group Butler Bot? (Y/N): "
+read -p "Do you want me to install Redolol Bot? (Y/N): "
 
 if [ "$REPLY" != "Y" ]; then
     echo "Exiting..."
@@ -22,20 +22,20 @@ else
     
     echo -e "\e[1;36mInstalling rocks\e[0m"
     
-    rocks="luasocket luasec redis-lua lua-term serpent dkjson Lua-cURL"
+    rocks="luasocket luasec redis-lua lua-term serpent dkjson Lua-cURL luautf8"
     for rock in $rocks; do
         sudo luarocks install $rock
     done
         
     branch="master"
-    read -p "Do you want to use the beta branch? (Y/N): "
+    read -p "Do you want to install redlol? (Y/N): "
     
     if [ "$REPLY" == "Y" ]; then
         branch="beta"
     fi
     
     echo -e "\e[1;36mFetching latest Group Butler source code (branch: $branch)\e[0m"
-    git clone -b $branch https://github.com/RememberTheAir/GroupButler.git
+    git clone -b $branch https://github.com/olhodedeus/Redlol.git
     
     echo -e "\e[1;32mGroup Butler successfully installed! Change values in config file and run ./launch.sh\e[0m"
     echo " "
